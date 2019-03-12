32aa87815d6e94a00f1ead951934c0e5760a81d1
federico de guio <federico.de.guio@cern.ch>
2019-03-12 11:21:43 +0100

    first commit

---
+++ main/PGunAnalysis.cpp
@@ -0,0 +1,30 @@
+#include "interface/SetTDRStyle.h"
+
+#include "CfgManager/interface/CfgManager.h"
+#include "CfgManager/interface/CfgManagerT.h"
+
+#include "TSystem.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TF1.h"
+#include "TCanvas.h"
+#include "TLatex.h"
+#include "TLine.h"
+#include "TApplication.h"
+
+
+
+int main(int argc, char** argv)
+{
+  gSystem -> Load("CfgManager/lib/libCFGMan.so");
+  setTDRStyle();
+
+  return 0;
+}
