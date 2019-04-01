85a00929fa34e8d2bfeec9d503b26dbcf342de35
federico de guio <federico.de.guio@cern.ch>
2019-04-01 12:42:18 +0200

    more scenarios and analysis improvements

--- main/PGunAnalysis.cpp
+++ main/PGunAnalysis.cpp
@@ -21,41 +21,42 @@
 #include "TLine.h"
 #include "TApplication.h"
 
-
-TGraphErrors* makeResGraph(TH2F* resol2d)
+//                                             minVal,   maxVal,   nBinsToMerge
+TGraphErrors* makeResGraph(TH2F* resol2d, float min, float max, int width)
 {
   TGraphErrors* outGraph = new TGraphErrors();
   int count=0;
-  for(int ii = 1; ii < resol2d->GetXaxis()->GetNbins()+1; ++ii)
-    {
-      //std::cout << "Bin " << ii << " of " << resol2d->GetXaxis()->GetNbins() << std::endl;
-      TH1F* tmpHisto = (TH1F*)resol2d->ProjectionY("tmpHisto",ii,ii);
-      if(tmpHisto->GetEntries() < 1)
-        continue;
+  for(int ii = resol2d->GetXaxis()->FindBin(min); ii < resol2d->GetXaxis()->FindBin(max)+1; ii+=width)
+  {
+    //std::cout << "Bin " << ii << " of " << resol2d->GetXaxis()->GetNbins() << std::endl;
+    TH1F* tmpHisto = (TH1F*)resol2d->ProjectionY((std::string(std::to_string(ii)+"_to_"+std::to_string(ii+width))).c_str(),ii,ii + width-1);
+    if(tmpHisto->GetEntries() < 1)
+      continue;
 
-      //tmpHisto->Rebin(rebin);
-      TF1* mygaus = new TF1("mygauss","gaus",-2, 2);
-      tmpHisto->Fit(mygaus,"QR");
+    //tmpHisto->Rebin(rebin);
+    TF1* mygaus = new TF1("mygauss","gaus",-2, 2);
+    tmpHisto->Fit(mygaus,"QR");
 
-      float rangeMin = mygaus->GetParameter(1) - 2*mygaus->GetParameter(2);
-      float rangeMax = mygaus->GetParameter(1) + 2*mygaus->GetParameter(2);
-      mygaus->SetRange(rangeMin,rangeMax);
-      tmpHisto->Fit(mygaus,"QR+");
+    float rangeMin = mygaus->GetParameter(1) - 2*mygaus->GetParameter(2);
+    float rangeMax = mygaus->GetParameter(1) + 2*mygaus->GetParameter(2);
+    mygaus->SetRange(rangeMin,rangeMax);
+    tmpHisto->Fit(mygaus,"QR+");
 
-      float sigma = mygaus->GetParameter(2);
-      outGraph->SetPoint(count, resol2d->GetXaxis()->GetBinCenter(ii), sigma);
-      outGraph->SetPointError(count, 0, mygaus->GetParError(2));
+    float sigma = mygaus->GetParameter(2);
+    float sigmaE = mygaus->GetParError(2);
+    outGraph->SetPoint(count, resol2d->GetXaxis()->GetBinCenter(ii), sigma);
+    outGraph->SetPointError(count, 0, sigmaE);
 
-      // float parstime[4];
-      // FindSmallestInterval(parstime, tmpHisto, 0.68);
-      // g_timeRes_vsAmp->SetPoint(ii-1,h_timeDiff_vsAmp->GetXaxis()->GetBinCenter(ii), (parstime[3]-parstime[2])/2);
+    // float parstime[4];
+    // FindSmallestInterval(parstime, tmpHisto, 0.68);
+    // g_timeRes_vsAmp->SetPoint(ii-1,h_timeDiff_vsAmp->GetXaxis()->GetBinCenter(ii), (parstime[3]-parstime[2])/2);
 
-      tmpHisto->Write();
-      ++count;
+    tmpHisto->Write();
+    ++count;
 
-      delete mygaus;
-      delete tmpHisto;
-    }
+    delete mygaus;
+    delete tmpHisto;
+  }
 
   return outGraph;
 }
@@ -70,6 +71,10 @@ int main(int argc, char** argv)
   CfgManager opts;
   opts.ParseConfigFile(argv[1]);
 
+  int nEntries = -1;
+  if(argc > 2)
+  nEntries = atoi(argv[2]);
+
   //ACCESS PARAMETERS
   std::string inputName = opts.GetOpt<std::string>("Input.inputFile");
   std::string outputPath = opts.GetOpt<std::string>("Input.outputFolder");
@@ -81,9 +86,10 @@ int main(int argc, char** argv)
   float mipRange = opts.GetOpt<float>("Params.mipRange");
   int nAdcBits = opts.GetOpt<int>("Params.nAdcBits");
   float zsThr = opts.GetOpt<float>("Params.zsThr");
+  std::vector<double> radiusRegions = opts.GetOpt<std::vector<double>>("Plot.radiusRegions");
   int nAdcChannels = pow(2, nAdcBits);
 
-  TApplication *theApp = new TApplication( "app", &argc, argv );
+  //TApplication *theApp = new TApplication( "app", &argc, argv );
 
   std::string createFolder = "mkdir -p "+outputPath;
   system(createFolder.c_str());
@@ -98,17 +104,23 @@ int main(int argc, char** argv)
   //book histograms
   TH1F* h_simHit_particle_deltaR = new TH1F("h_simHit_particle_deltaR","h_simHit_particle_deltaR",100,0,1);
   TH1F* h_simHit_digi_diff = new TH1F("h_simHit_digi_diff","h_simHit_digi_diff",200,-5,5);
-  TH1F* h_simHit_energy = new TH1F("h_simHit_energy","h_simHit_energy",500, 0, 0.005);
+  TH1F* h_simHit_energy = new TH1F("h_simHit_energy","h_simHit_energy",150, 0, 10);
 
   TH1F* h_digi_energy = new TH1F("h_digi_energy","h_digi_energy",150, 0, 10);
-  TH1F* h_ped_energy = new TH1F("h_ped_energy","h_ped_energy",1000, 0, 100);
+  TH1F* h_ped_energy = new TH1F("h_ped_energy","h_ped_energy",150, 0, 10);
+
+  TProfile* h_pulseShape = new TProfile("h_pulseShape","h_pulseShape", 5, 0., 5.);
 
   //resolution plots
-  TH2F* h_enDiff_vs_ieta = new TH2F("h_enDiff_vs_ieta","h_enDiff_vs_ieta",50, 0, 50, 100, -5, 5);
+  TH2F* h_enDiff_vs_irad = new TH2F("h_enDiff_vs_irad","h_enDiff_vs_irad",50, 0, 50, 200, -5, 5);
   TH2F* h_enDiff_vs_layer = new TH2F("h_enDiff_vs_layer","h_enDiff_vs_layer",30, 0, 30, 100, -5, 5);
+  std::map<double, TH2F*> hMap_enDiff_vs_layer;
+  for(unsigned int bin=0; bin<radiusRegions.size()-1; ++bin)
+    hMap_enDiff_vs_layer[radiusRegions[bin]] = new TH2F(Form("h_enDiff_vs_layer_%f-%f",radiusRegions[bin],radiusRegions[bin+1]),"",30, 0, 30, 100, -5, 5);
 
   //loop over entries
-  int nEntries = chain -> GetEntries();
+  if(nEntries == -1)
+  nEntries = chain -> GetEntries();
   for(int entry = 0; entry < nEntries; ++entry)
   {
     if(entry % 100 == 0)
@@ -121,58 +133,83 @@ int main(int argc, char** argv)
     double genPhi = tt.GenParPhi->at(0);
 
     //loop over ALL SimHits
-//    for(unsigned int simHit=0; simHit<tt.HGCSimHitsEta->size(); ++simHit)
-//    {
-//      double simHitEta = tt.HGCSimHitsEta->at(simHit);
-//      double simHitPhi = tt.HGCSimHitsPhi->at(simHit);
-//
-//      double simHit_particle_deltaR = sqrt( deltaR2(genEta,genPhi,simHitEta,simHitPhi) );
-//      h_simHit_particle_deltaR->Fill(simHit_particle_deltaR);
-//    }
+    //    for(unsigned int simHit=0; simHit<tt.HGCSimHitsEta->size(); ++simHit)
+    //    {
+    //      double simHitEta = tt.HGCSimHitsEta->at(simHit);
+    //      double simHitPhi = tt.HGCSimHitsPhi->at(simHit);
+    //
+    //      double simHit_particle_deltaR = sqrt( deltaR2(genEta,genPhi,simHitEta,simHitPhi) );
+    //      h_simHit_particle_deltaR->Fill(simHit_particle_deltaR);
+    //    }
 
-    //loop over summed simHits
-    for(unsigned int simHit=0; simHit<tt.HGCSimHitsIntIEta->size(); ++simHit)
+    //loop over digis
+    for(unsigned int digi=0; digi<tt.HGCDigiIEta->size(); ++digi)
     {
-      //select the detector index
-      double simHitIndex = tt.HGCSimHitsIntIndex->at(simHit);
-      if(simHitIndex != detIndex)
+      int digiIndex = tt.HGCDigiIndex->at(digi);
+      //consider only the relevant subdet
+      if(digiIndex != detIndex)
         continue;
 
-      float simHitEn = tt.HGCSimHitsIntEnergy->at(simHit);
-      int simHitIEta = tt.HGCSimHitsIntIEta->at(simHit);
-      int simHitIPhi = tt.HGCSimHitsIntIPhi->at(simHit);
-      int simHitLayer = tt.HGCSimHitsIntLayer->at(simHit);
+      int digiIRad = tt.HGCDigiIEta->at(digi);
+      int digiIPhi = tt.HGCDigiIPhi->at(digi);
+      int digiLayer = tt.HGCDigiLayer->at(digi);
+      float ped = (tt.HGCDigiSamples->at(digi)[0] + tt.HGCDigiSamples->at(digi)[1])/2;
+      float sig = tt.HGCDigiSamples->at(digi)[2];
 
-      //to make a fair comparison with the DIGIs
-      if(simHitEn/gevPerMip < zsThr)
+      //transform to Mips
+      sig = sig / nAdcChannels * mipRange;
+      ped = ped / nAdcChannels * mipRange;
+
+      //apply ZS on digis before subtracting ped
+      if(sig <= zsThr)
         continue;
 
-      h_simHit_energy->Fill(simHitEn);
+      //subtract the ped
+      sig = sig - ped;
 
-      for(unsigned int digi=0; digi<tt.HGCDigiIEta->size(); ++digi)
+      //loop over summed simHits
+      for(unsigned int simHit=0; simHit<tt.HGCSimHitsIntIEta->size(); ++simHit)
       {
-        int digiIEta = tt.HGCDigiIEta->at(digi);
-        int digiIPhi = tt.HGCDigiIPhi->at(digi);
-        int digiLayer = tt.HGCDigiLayer->at(digi);
-        int digiIndex = tt.HGCDigiIndex->at(digi);
+        //select the detector index
+        double simHitIndex = tt.HGCSimHitsIntIndex->at(simHit);
+        if(simHitIndex != detIndex)
+          continue;
+
+        int simHitIRad = tt.HGCSimHitsIntIEta->at(simHit);
+        int simHitIPhi = tt.HGCSimHitsIntIPhi->at(simHit);
+        int simHitLayer = tt.HGCSimHitsIntLayer->at(simHit);
+        float simHitEn = tt.HGCSimHitsIntEnergy->at(simHit);
+
+        //transform to Mips
+        simHitEn = simHitEn / gevPerMip;
 
         //DetID matching
-        if(simHitIEta==digiIEta && simHitIPhi==digiIPhi && simHitLayer==digiLayer && simHitIndex==digiIndex)
+        if(simHitIRad==digiIRad && simHitIPhi==digiIPhi && simHitLayer==digiLayer)
         {
-          float ped = (tt.HGCDigiSamples->at(digi)[0] + tt.HGCDigiSamples->at(digi)[1])/2;
-          float sig = tt.HGCDigiSamples->at(digi)[2] - ped;
+          //fill pulse shape plot
+          for(unsigned int sampl = 0; sampl<tt.HGCDigiSamples->at(digi).size(); ++sampl)
+            h_pulseShape->Fill(sampl, tt.HGCDigiSamples->at(digi)[sampl]);
 
-          //transform to Mip
-          sig = sig / nAdcChannels * mipRange;
-          simHitEn = simHitEn / gevPerMip;
 
           h_digi_energy->Fill(sig);
           h_ped_energy->Fill(ped);
+          h_simHit_energy->Fill(simHitEn);
 
           float diff = sig - simHitEn;
           h_simHit_digi_diff->Fill(diff);
-          h_enDiff_vs_ieta->Fill(std::abs(digiIEta), diff);
-          h_enDiff_vs_layer->Fill(digiLayer, diff);
+          h_enDiff_vs_irad->Fill(std::abs(digiIRad), diff/simHitEn);
+          h_enDiff_vs_layer->Fill(digiLayer,         diff/simHitEn);
+
+          //fill histo map
+          for(auto itr=hMap_enDiff_vs_layer.rbegin(); itr!=hMap_enDiff_vs_layer.rend(); ++itr)
+          {
+            if(std::abs(digiIRad)>itr->first)
+            {
+              itr->second->Fill(digiLayer, diff/simHitEn);
+              break;
+            }
+          }
+
           break;
         }
       }
@@ -181,54 +218,35 @@ int main(int argc, char** argv)
   std::cout << std::endl;
 
   //compute resolution
-  TDirectory* dirIEta = outFile->mkdir("resVsIEta");
-  dirIEta->cd();
-  TGraphErrors* g_res_vs_ieta = makeResGraph(h_enDiff_vs_ieta);
-  g_res_vs_ieta->SetName("g_res_vs_ieta");
-  g_res_vs_ieta->GetXaxis()->SetTitle("IEta");
-  g_res_vs_ieta->GetYaxis()->SetTitle("#sigma(reco - gen) [MIP]");
-
-  TCanvas* c_res_vs_ieta = new TCanvas();
-  g_res_vs_ieta->Draw("AP");
-  c_res_vs_ieta->Update();
+  TDirectory* dirIRad = outFile->mkdir("resVsIRad");
+  dirIRad->cd();
+  TGraphErrors* g_res_vs_irad = makeResGraph(h_enDiff_vs_irad, 0, 50, 2);
+  g_res_vs_irad->SetName("g_res_vs_irad");
+  g_res_vs_irad->GetXaxis()->SetTitle("IRad");
+  g_res_vs_irad->GetYaxis()->SetTitle("#sigma(reco - gen) / gen");
   outFile->cd();
-  g_res_vs_ieta->Write();
+  g_res_vs_irad->Write();
 
   TDirectory* dirLayer = outFile->mkdir("resVsLayer");
   dirLayer->cd();
-  TGraphErrors* g_res_vs_layer = makeResGraph(h_enDiff_vs_layer);
+  TGraphErrors* g_res_vs_layer = makeResGraph(h_enDiff_vs_layer, 8, 26, 1);
   g_res_vs_layer->SetName("g_res_vs_layer");
   g_res_vs_layer->GetXaxis()->SetTitle("layer");
-  g_res_vs_layer->GetYaxis()->SetTitle("#sigma(reco - gen) [MIP]");
-
-  TCanvas* c_res_vs_layer = new TCanvas();
-  g_res_vs_layer->Draw("AP");
-  c_res_vs_layer->Update();
+  g_res_vs_layer->GetYaxis()->SetTitle("#sigma(reco - gen) / gen");
   outFile->cd();
   g_res_vs_layer->Write();
 
-
-  //plot
-  TCanvas* c_simHit_energy = new TCanvas();
-  h_simHit_energy->Draw();
-  c_simHit_energy->Update();
-
-  TCanvas* c_simHit_particle_deltaR = new TCanvas();
-  h_simHit_particle_deltaR->Draw();
-  c_simHit_particle_deltaR->Update();
-
-  TCanvas* c_enDiff_vs_ieta = new TCanvas();
-  h_enDiff_vs_ieta->Draw("COLZ");
-  c_enDiff_vs_ieta->Update();
-
-  TCanvas* c_enDiff_vs_layer = new TCanvas();
-  h_enDiff_vs_layer->Draw("COLZ");
-  c_enDiff_vs_layer->Update();
-
-
-  TCanvas* c_simHit_digi_diff = new TCanvas();
-  h_simHit_digi_diff->Draw();
-  c_simHit_digi_diff->Update();
+  for(auto itr=hMap_enDiff_vs_layer.begin(); itr!=hMap_enDiff_vs_layer.end(); ++itr)
+  {
+    TDirectory* dir = outFile->mkdir(Form("resVsLayer_%f",itr->first));
+    dir->cd();
+    g_res_vs_layer = makeResGraph(itr->second, 8, 26, 1);
+    g_res_vs_layer->SetName(Form("g_res_vs_layer_%f",itr->first));
+    g_res_vs_layer->GetXaxis()->SetTitle("layer");
+    g_res_vs_layer->GetYaxis()->SetTitle("#sigma(reco - gen) / gen");
+    outFile->cd();
+    g_res_vs_layer->Write();
+  }
 
 
   //theApp -> Run();
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,8 +1,7 @@
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_0_algo_0_scaleByArea_False_scaleByDose_False.cfg >& /tmp/deguio/1.txt &
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_0_algo_1_scaleByArea_False_scaleByDose_False.cfg >& /tmp/deguio/2.txt &
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_0_algo_2_scaleByArea_False_scaleByDose_False.cfg >& /tmp/deguio/3.txt &
+#time ./bin/PGunAnalysis.exe cfg/noiseScenario_0_algo_0_scaleByArea_False_scaleByDose_False.cfg >& /tmp/deguio/1.txt &
+time ./bin/PGunAnalysis.exe cfg/20190330/noiseScenario_0_algo_1_scaleByArea_False.cfg >& /tmp/deguio/2.txt &
+time ./bin/PGunAnalysis.exe cfg/20190330/noiseScenario_0_algo_2_scaleByArea_False.cfg >& /tmp/deguio/3.txt &
+time ./bin/PGunAnalysis.exe cfg/20190330/noiseScenario_0_algo_2_scaleByArea_True.cfg  >& /tmp/deguio/4.txt &
 
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_3000_algo_1_scaleByArea_False_scaleByDose_False.cfg >& /tmp/deguio/5.txt &
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_3000_algo_2_scaleByArea_False_scaleByDose_False.cfg >& /tmp/deguio/6.txt &
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_3000_algo_2_scaleByArea_True_scaleByDose_False.cfg >& /tmp/deguio/7.txt &
-time ./bin/PGunAnalysis.exe cfg/noiseScenario_3000_algo_2_scaleByArea_True_scaleByDose_True.cfg >& /tmp/deguio/8.txt &
+time ./bin/PGunAnalysis.exe cfg/20190330/noiseScenario_3000_algo_2_scaleByArea_False.cfg >& /tmp/deguio/5.txt &
+time ./bin/PGunAnalysis.exe cfg/20190330/noiseScenario_3000_algo_2_scaleByArea_True.cfg >& /tmp/deguio/6.txt &
